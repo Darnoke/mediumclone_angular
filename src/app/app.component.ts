fd6c22bad43d84233b035d3cb6da04cfe4b49771
Konrad <konradwozniak64@gmail.com>
2024-03-09 09:12:47 +0100

    Added topBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBar } from './shared/components/topBar/topBar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBar],
 })
 export class AppComponent {
   title = 'mediumclone_angular';
