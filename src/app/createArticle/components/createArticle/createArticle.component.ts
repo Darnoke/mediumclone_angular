310cb684bfc2aa338b6eafa16d1545ab5ffd3572
Konrad <konradwozniak64@gmail.com>
2024-03-24 12:31:32 +0100

    Added create article store

--- src/app/createArticle/components/createArticle/createArticle.component.ts
+++ src/app/createArticle/components/createArticle/createArticle.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleFormValuesInterface } from 'src/app/shared/components/articleForm/types/articleFormValues.interface';
 import { ArticleFormComponent } from 'src/app/shared/components/articleForm/articleForm.component';
+import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { ArticleRequestInterface } from 'src/app/shared/types/articleRequest.interface';
+import { createArticleActions } from '../../store/actions';
 
 @Component({
   selector: 'mc-create-article',
@@ -17,7 +25,17 @@ export class CreateArticleComponent {
     tagList: [],
   };
 
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+
+  constructor(private store: Store) {}
+
   onSubmit(articleFormValues: ArticleFormValuesInterface): void {
-    console.log(articleFormValues);
+    const request: ArticleRequestInterface = {
+      article: articleFormValues,
+    };
+    this.store.dispatch(createArticleActions.createArticle({ request }));
   }
 }
