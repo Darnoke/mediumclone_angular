73a872003d5477b23219da8c9d2b1d8897decd89
Konrad <konradwozniak64@gmail.com>
2024-03-25 11:23:05 +0100

    Added settings page

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -63,6 +63,11 @@ const authFeature = createFeature({
       ...state,
       currentUser: action.currentUser,
     })),
+    on(authActions.logout, (state) => ({
+      ...state,
+      ...initialState,
+      currentUser: null,
+    })),
     on(routerNavigationAction, (state) => ({
       ...state,
       validationErrors: null,
